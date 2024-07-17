3b5af4ed8915f7aa1d90cfa794d6f06cd193fd2c
Hassan Javaid <scuderia.hj@gmail.com>
2024-07-17 12:40:14 +0500

    Updated logo and favicon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/joysitck-ai-generated-webp.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
@@ -10,7 +10,7 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize={"60px"} />
+      <Image src={logo} boxSize={"90px"} />
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
